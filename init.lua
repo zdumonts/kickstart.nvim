15577a8f04204fb45de69caabb1ce1b47f68932e
Zander <zdumonts@ucsc.edu>
2024-05-11 21:12:38 +0000

    add .vimrc to nvim config

--- init.lua
+++ init.lua
@@ -823,5 +823,9 @@ require('lazy').setup({
   },
 })
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+vim.cmd([[
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc
+]])
+
